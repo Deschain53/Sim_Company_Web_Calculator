b6ceef4cc0dc2857199aebb8e73138c2dbb03023
Deschain53 <jorgeadrianlucassanchez@gmail.com>
2021-09-24 22:36:03 -0500

    The spanish instructions was begin but still need to be completed and implemented

---
+++ src/languaje/instructions/info/spanish_instructions.js
@@ -0,0 +1,121 @@
+export const spanish_instruction = {
+    productionTitle: 'Instrucciones en español',
+    productionText: '',
+    productionArray = [
+        {
+            id: 1,
+            title: 'Presione el botó "Get prices" ',
+            text: 'Ésta acción obtendrá los precios desde el mercado usando la API de Sim Company y los guardará en tu navegador',
+        },
+        {
+            id: 2,
+            title: 'Escribe la información requerida',
+            text: 'Todos los numeros deberán ser positivos. Si cometes un error el valor por default será 0',
+        },
+        {
+            id: 3,
+            title: 'Selecciona el edificio, fase y calidad',
+            text: 'Cuando cambies alguno de éstos valores los cambios se aplicarán de inmediato',
+        },
+        {
+            id: 4,
+            title: 'Presiona el botón "Calcular"',
+            text: 'Ésto aplicará y guardará todos los cambios',
+        },
+    ],
+
+    considerationsTitle: 'Algunas consideraciones',
+    considerationsText: '',
+    considerationsArray: [
+        {
+            id: 1,
+            text: '',
+        },
+    ],
+
+    partsProductionTitle: 'Partes de la calculadora de producción',
+    columsProductionArray: [
+        {
+            id: 1,
+            title: '',
+            text: '',
+        },
+        {
+            id: 2,
+            title: '',
+            text: '',
+        },
+        {
+            id: 3,
+            title: '',
+            text: '',
+        },
+        {
+            id: 4,
+            title: '',
+            text: '',
+        },
+        {
+            id: 5,
+            title: '',
+            text: '',
+        },
+        {
+            id: 6,
+            title: '',
+            text: '',
+        },
+        {
+            id: 7,
+            title: '',
+            text: '',
+        },
+        {
+            id: 8,
+            title: '',
+            text: '',
+        },
+        {
+            id: 9,
+            title: '',
+            text: '',
+        },
+        {
+            id: 10,
+            title: '',
+            text: '',
+        }, 
+    ],
+    rowsProductionArray: [
+        {
+            id: 'A',
+            title: '',
+            text: '',
+        },
+        {
+            id: 'a',
+            title: '',
+            text: '',
+        },
+        {
+            id: 'b',
+            title: '',
+            text: '',
+        },
+        {
+            id: 'c',
+            title: '',
+            text: '',
+        },
+        {
+            id: 'd',
+            title: '',
+            text: '',
+        },
+        {
+            id: 'e',
+            title: '',
+            text: '',
+        },
+    ],
+}
