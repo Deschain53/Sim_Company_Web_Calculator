966924a87092f28d1edb381abb058ad6010320d3
Deschain53 <jorgeadrianlucassanchez@gmail.com>
2021-10-31 23:03:11 -0600

    Style fixed

--- src/components/ui/ProductionTable/TableHeadMain.js
+++ src/components/ui/ProductionTable/TableHeadMain.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { IconButton, TableHead, TableRow } from '@material-ui/core'
+import { IconButton, TableHead, TableRow } from '@material-ui/core';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import { StyledTableCell } from '../../../styles/material-ui-styles/tableStyles'
 import { tableProduction_index } from '../../../languaje/tables/tableProduction/tableProduction_index'
 
-
 export const TableHeadMain = ({open, setOpen}) => {
+
   const { languaje } = useSelector(state => state.conf);
+  const {mode} = useSelector(state => state.conf);
 
   const infoTable = tableProduction_index[`${languaje}_tableP`];
 
-  const mode ='dark';
     return (
         <TableHead>
           <TableRow>
             <StyledTableCell mode={mode}>
-              <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)} color={ mode === 'dark' ? "primary" : "default" }>
+              <IconButton 
+                aria-label="expand row" 
+                size="small" 
+                onClick={() => setOpen(!open)} 
+                color={ mode === 'dark' ? "primary" : "inherit" }
+                >
                 {open ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />}
               </IconButton>
             </StyledTableCell>
