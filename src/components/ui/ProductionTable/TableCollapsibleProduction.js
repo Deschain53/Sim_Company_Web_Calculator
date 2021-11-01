7080f4887f11e7cb5096be80970b6b393c152084
Deschain53 <jorgeadrianlucassanchez@gmail.com>
2021-10-31 22:54:20 -0600

    Added action to order the table

--- src/components/ui/ProductionTable/TableCollapsibleProduction.js
+++ src/components/ui/ProductionTable/TableCollapsibleProduction.js
@@ -1,13 +1,53 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { Table, TableBody, TableContainer, } from '@material-ui/core'
 import Paper from '@material-ui/core/Paper';
 import { TableHeadMain } from './TableHeadMain';
 import { Row } from './Rows/Row';
 import { useCalculaProduction } from '../../../hooks/useCalculaProduction'
 
+const orderArrayDescendantByProfitHourContract = (array) => {
+  array.sort(function (a, b) {
+    if (a.profitHourContract > b.profitHourContract) {
+      return 1;
+    }
+    if (a.profitHourContract < b.profitHourContract) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  })
+};
+
+const orderArrayAscendantByProfitHourContract = (array) => {
+  array.sort(function (a, b) {
+    if (a.profitHourContract > b.profitHourContract) {
+      return -1;
+    }
+    if (a.profitHourContract < b.profitHourContract) {
+      return 1;
+    }
+    // a must be equal to b
+    return 0;
+  })
+};
+
 export const TableCollapsibleProduction = () => {
   const [open, setOpen] = useState(false);
   const {tableP} = useCalculaProduction();
+
+  useMemo(() => {
+    if(open){
+      orderArrayAscendantByProfitHourContract(tableP);
+    }else{
+      orderArrayDescendantByProfitHourContract(tableP);
+    }
+  // eslint-disable-next-line
+  }, [open])
+
+  useEffect(() => {
+    setOpen(true);
+  }, [])
+
   
   return (
     <TableContainer component={Paper}>
